005f4effdd0595f6a7e71f81c996f439ecceb9a1
Aiden Fox Ivey <aiden@fox-ivey.com>
2024-03-17 10:32:55 -0400

    Fix lexer types
    Signed-off-by: Aiden Fox Ivey <aiden@fox-ivey.com>

--- goterpreter/lexer.go
+++ goterpreter/lexer.go
@@ -1,43 +1,45 @@
 package main
 
+import "fmt"
+
 type Expr interface {
 	Evaluate() interface{}
 	Visit() string
 }
 
 type Binary struct {
-	left  *Expr
+	left  Expr
 	op    LoxToken
-	right *Expr
+	right Expr
 }
 
-func (b *Binary) Evaluate() {
+func (b *Binary) Evaluate() interface{} {
 	return b.left.Evaluate()
 }
 
 func (b *Binary) Visit() string {
-	return fmt.Sprintf("(%s %s %s)", left.Visit(), op.String(), right.Visit())
+	return fmt.Sprintf("(%s %s %s)", b.left.Visit(), b.op.String(), b.right.Visit())
 }
 
 type Unary struct {
 	op    LoxToken
-	right *Expr
+	right Expr
 }
 
-func (u *Unary) Evaluate() {
-	return b.right.Evaluate()
+func (u *Unary) Evaluate() interface{} {
+	return u.right.Evaluate()
 }
 
 func (u *Unary) Visit() string {
-	return fmt.Sprintf("(%s %s)", op.String(), right.Visit())
+	return fmt.Sprintf("(%s %s)", u.op.String(), u.right.Visit())
 }
 
 type Literal struct {
 	value interface{}
 }
 
-func (l *Literal) Evaluate() {
-	return value
+func (l *Literal) Evaluate() interface{} {
+	return l.value
 }
 
 func (l *Literal) Visit() string {
@@ -45,13 +47,13 @@ func (l *Literal) Visit() string {
 }
 
 type Grouping struct {
-	expression *Expr
+	expression Expr
 }
 
-func (g *Grouping) Evaluate() {
-	return expression.Evaluate()
+func (g *Grouping) Evaluate() interface{} {
+	return g.expression.Evaluate()
 }
 
 func (g *Grouping) Visit() string {
-	return fmt.Sprintf("%s", expr.Visit())
+	return fmt.Sprintf("%s", g.expression.Visit())
 }
