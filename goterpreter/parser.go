74532d4dff0821901813d4e9c8e1109ef6d7a7be
Aiden Fox Ivey <aiden@fox-ivey.com>
2024-03-17 16:08:35 -0400

    Add unary()
    Signed-off-by: Aiden Fox Ivey <aiden@fox-ivey.com>

--- goterpreter/parser.go
+++ goterpreter/parser.go
@@ -9,29 +9,75 @@ func NewParser(tokens []LoxToken) *Parser {
 	return &Parser{tokens, 0}
 }
 
-func (p *Parser) expression() *Expr {
+func (p *Parser) expression() Expr {
 	return p.equality()
 }
 
-func (p *Parser) equality() *Expr {
-	expr := comparison()
+func (p *Parser) equality() Expr {
+	expr := p.comparison()
 
 	for {
-		if !match(BangEqual, EqualEqual) {
+		if !p.match([]TokenType{BangEqual, EqualEqual}) {
 			break
 		}
-		operator := previous()
-		right := comparison()
-		expr = Binary{left: expr, op: operator, right: right}
+		operator := p.previous()
+		right := p.comparison()
+		expr = Binary{left: expr, operator: operator, right: right}
 	}
 
 	return expr
 }
 
-func (p *Parser) match(types []LoxToken) bool {
+func (p *Parser) comparison() Expr {
+	expr := p.term()
+
+	for {
+		if !p.match([]TokenType{Greater, GreaterEqual, Less, LessEqual}) {
+			break
+		}
+		operator := p.previous()
+		right := p.term()
+		expr = Binary{left: expr, operator: operator, right: right}
+	}
+
+	return expr
+}
+
+func (p *Parser) term() Expr {
+	expr := p.factor()
+
+	for {
+		if !p.match([]TokenType{Minus, Plus}) {
+			break
+		}
+
+		operator := p.previous()
+		right := p.factor()
+		expr = Binary{left: expr, operator: operator, right: right}
+	}
+
+	return expr
+}
+
+func (p *Parser) factor() Expr {
+	expr := p.unary()
+
+	for {
+		if !p.match([]TokenType{Slash, Star}) {
+			break
+		}
+		operator := p.previous()
+		right := p.unary()
+		expr = Binary{left: expr, operator: operator, right: right}
+	}
+
+	return expr
+}
+
+func (p *Parser) match(types []TokenType) bool {
 	for _, t := range types {
-		if check(t) {
-			advance()
+		if p.check(t) {
+			p.advance()
 			return true
 		}
 	}
@@ -39,12 +85,12 @@ func (p *Parser) match(types []LoxToken) bool {
 	return false
 }
 
-func (p *Parser) check(TokenType t) bool {
+func (p *Parser) check(t TokenType) bool {
 	if p.isAtEnd() {
 		return false
 	}
 
-	return t == peek()
+	return t == p.peek().Type
 }
 
 func (p *Parser) advance() LoxToken {
@@ -55,7 +101,7 @@ func (p *Parser) advance() LoxToken {
 }
 
 func (p *Parser) isAtEnd() bool {
-	return peek() == EOF
+	return p.peek().Type == Eof
 }
 
 func (p *Parser) peek() LoxToken {
@@ -64,4 +110,14 @@ func (p *Parser) peek() LoxToken {
 
 func (p *Parser) previous() LoxToken {
 	return p.tokens[p.current-1]
+}
+
+func (p *Parser) unary() Expr {
+	if match([]TokenType{Bang, Minus}) {
+		operator := p.previous()
+		right := p.unary()
+		return Unary{operator: operator, right: right}
+	}
+
+	return p.primary()
 }
