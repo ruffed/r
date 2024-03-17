bdbf841200c78e358f145d55948dc671c6a2dba2
Aiden Fox Ivey <aiden@fox-ivey.com>
2024-03-16 23:51:56 -0400

    Add rudimentary AST
    The file name is wrong - please accept that for now.
    
    With this, chapter 5 is done.
    
    Signed-off-by: Aiden Fox Ivey <aiden@fox-ivey.com>

--- goterpreter/lexer.go
+++ goterpreter/lexer.go
@@ -1,7 +1,57 @@
 package main
 
-type Expr struct {
+type Expr interface {
+	Evaluate() interface{}
+	Visit() string
+}
+
+type Binary struct {
 	left  *Expr
 	op    LoxToken
 	right *Expr
+}
+
+func (b *Binary) Evaluate() {
+	return b.left.Evaluate()
+}
+
+func (b *Binary) Visit() string {
+	return fmt.Sprintf("(%s %s %s)", left.Visit(), op.String(), right.Visit())
+}
+
+type Unary struct {
+	op    LoxToken
+	right *Expr
+}
+
+func (u *Unary) Evaluate() {
+	return b.right.Evaluate()
+}
+
+func (u *Unary) Visit() string {
+	return fmt.Sprintf("(%s %s)", op.String(), right.Visit())
+}
+
+type Literal struct {
+	value interface{}
+}
+
+func (l *Literal) Evaluate() {
+	return value
+}
+
+func (l *Literal) Visit() string {
+	return fmt.Sprintf("%v", l.value)
+}
+
+type Grouping struct {
+	expression *Expr
+}
+
+func (g *Grouping) Evaluate() {
+	return expression.Evaluate()
+}
+
+func (g *Grouping) Visit() string {
+	return fmt.Sprintf("%s", expr.Visit())
 }
